5609fdd9fe648654aa15ec99075b6941432cfb1b
Franz Bowman <namwoblinus@gmail.com>
2016-05-26 15:44:33 -0500

    Initial Build

---
+++ src/converter/Convert.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter;
+
+/**
+ *
+ * @author bowmanFS
+ */
+public class Convert {
+    
+    //instance fields
+    private double celcius;
+    private double fahrenheit;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        calculate();
+    }
+
+    private static void calculate() {
+        //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
